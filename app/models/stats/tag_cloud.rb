ce67f3ed2de0e2f4044f90bf23cfeac4e392b4e2
ruben <rubengil22@gmail.com>
2013-09-30 10:01:58 +0200

    Extracting 90 days sql query

--- app/models/stats/tag_cloud.rb
+++ app/models/stats/tag_cloud.rb
@@ -33,7 +33,24 @@ class TagCloud
 
     @divisor = ((max - @min) / levels) + 1
 
-    # Get the tag cloud for all tags for actions
+    @tags_90days = Tag.find_by_sql(
+        [sql_90days, user.id, @cutt_off, @cutt_off]
+    ).sort_by { |tag| tag.name.downcase }
+
+    max_90days, @min_90days = 0, 0
+    @tags_90days.each { |t|
+      max_90days = [t.count.to_i, max_90days].max
+      @min_90days = [t.count.to_i, @min_90days].min
+    }
+
+    @divisor_90days = ((max_90days - @min_90days) / levels) + 1
+
+
+  end
+
+  private
+
+  def sql_90days
     query = "SELECT tags.id, tags.name AS name, count(*) AS count"
     query << " FROM taggings, tags, todos"
     query << " WHERE tags.id = tag_id"
@@ -45,16 +62,5 @@ class TagCloud
     query << " GROUP BY tags.id, tags.name"
     query << " ORDER BY count DESC, name"
     query << " LIMIT 100"
-    @tags_90days = Tag.find_by_sql(
-        [query, user.id, @cutt_off, @cutt_off]
-    ).sort_by { |tag| tag.name.downcase }
-
-    max_90days, @min_90days = 0, 0
-    @tags_90days.each { |t|
-      max_90days = [t.count.to_i, max_90days].max
-      @min_90days = [t.count.to_i, @min_90days].min
-    }
-
-    @divisor_90days = ((max_90days - @min_90days) / levels) + 1
   end
 end
